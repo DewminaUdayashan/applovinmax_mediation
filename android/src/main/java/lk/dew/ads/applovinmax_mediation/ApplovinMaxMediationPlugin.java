59816e436e80337d1d8c59fecbac47758c182a43
Dewmina Udayashan <dewminaudayashan@gmail.com>
2022-05-12 01:14:59 +0530

    (#13)

--- android/src/main/java/lk/dew/ads/applovinmax_mediation/ApplovinMaxMediationPlugin.java
+++ android/src/main/java/lk/dew/ads/applovinmax_mediation/ApplovinMaxMediationPlugin.java
@@ -80,8 +80,8 @@ public class ApplovinMaxMediationPlugin implements FlutterPlugin, MethodCallHand
 
 
     public void callback(String adUnitId, String callback, HashMap<String, String> error, ApplovinMaxMediationPlugin ins) {
-        Log.d(TAG, "callback: CALLBACK METHOD CALLED.... unit id : " + (adUnitId) + ", callback : " + (callback) + "," +
-                " error is null : " + (error == null) + ", is channel null ? :- " + (instance.channel == null));
+//        Log.d(TAG, "callback: CALLBACK METHOD CALLED.... unit id : " + (adUnitId) + ", callback : " + (callback) + "," +
+//                " error is null : " + (error == null) + ", is channel null ? :- " + (instance.channel == null));
         final HashMap<String, Object> data = new HashMap<>();
         data.put("callback", callback);
 //        if (error != null) {
