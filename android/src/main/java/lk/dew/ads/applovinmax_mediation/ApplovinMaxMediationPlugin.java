89c6b3edd7e110bc9f27c7b9bccc906aa1d5e3b1
Dewmina Udayashan <dewminaudayashan@gmail.com>
2022-05-11 22:47:03 +0530

    banner ad listner bug fix (#6)

--- android/src/main/java/lk/dew/ads/applovinmax_mediation/ApplovinMaxMediationPlugin.java
+++ android/src/main/java/lk/dew/ads/applovinmax_mediation/ApplovinMaxMediationPlugin.java
@@ -57,6 +57,7 @@ public class ApplovinMaxMediationPlugin implements FlutterPlugin, MethodCallHand
   private void initApplovinSDK(@NonNull Result result) {
     AppLovinSdk.getInstance(context).setMediationProvider( "max" );
     AppLovinSdk.initializeSdk(context, config -> {
+      callback("1","callbacks working file",null);
       if ( config.getConsentDialogState() == AppLovinSdkConfiguration.ConsentDialogState.APPLIES )
       {
         // Show user consent dialog
@@ -83,6 +84,7 @@ public class ApplovinMaxMediationPlugin implements FlutterPlugin, MethodCallHand
     if(error!=null){
       data.put("error",error);
     }
+    if(channel!=null)
     channel.invokeMethod(adUnitId,data);
   }
 
