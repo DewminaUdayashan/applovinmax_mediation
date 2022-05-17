a6e3b9341228afa8c9fc9f115200dc6f1c084468
Dewmina Udayashan <dewminaudayashan@gmail.com>
2022-05-17 11:53:41 +0530

    refactored (#25)

--- android/src/main/java/lk/dew/ads/applovinmax_mediation/ApplovinMaxMediationPlugin.java
+++ android/src/main/java/lk/dew/ads/applovinmax_mediation/ApplovinMaxMediationPlugin.java
@@ -34,7 +34,6 @@ public class ApplovinMaxMediationPlugin implements FlutterPlugin, MethodCallHand
     public Activity activity;
     public FlutterPluginBinding bindingInstance;
     private InterAd interstitialAd;
-    private RewardedAd rewardedAd;
 
     public ApplovinMaxMediationPlugin() {
         Log.d(TAG, "================ Applovin Mediation Plugin Initialized ================");
@@ -70,12 +69,6 @@ public class ApplovinMaxMediationPlugin implements FlutterPlugin, MethodCallHand
                 result.success(isInterReady());
             case "showInterstitialAd":
                 showInter(call.arguments);
-            case "showRewardedAd":
-                rewardedAd.showRewardedAd();
-            case "createRewardedAd":
-                rewardedAd.createRewardedAd(call.arguments.toString(), activity);
-            case "isRewardedAdReady":
-                result.success(rewardedAd.isReady());
             default:
                 result.notImplemented();
                 break;
@@ -168,7 +161,6 @@ public class ApplovinMaxMediationPlugin implements FlutterPlugin, MethodCallHand
         activity = binding.getActivity();
         channel.setMethodCallHandler(this);
         interstitialAd = new InterAd(instance);
-        rewardedAd = new RewardedAd(instance);
     }
 
     @Override
--- android/src/main/java/lk/dew/ads/applovinmax_mediation/RewardedAd.java
+++ android/src/main/java/lk/dew/ads/applovinmax_mediation/RewardedAd.java
@@ -1,115 +1,115 @@
-package lk.dew.ads.applovinmax_mediation;
-
-import android.app.Activity;
-import android.os.Handler;
-
-import androidx.annotation.NonNull;
-
-import com.applovin.mediation.MaxAd;
-import com.applovin.mediation.MaxError;
-import com.applovin.mediation.MaxReward;
-import com.applovin.mediation.MaxRewardedAdListener;
-import com.applovin.mediation.ads.MaxRewardedAd;
-
-import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
-public class RewardedAd
-        implements MaxRewardedAdListener {
-    private MaxRewardedAd rewardedAd;
-    private int retryAttempt;
-    private ApplovinMaxMediationPlugin instance;
-
-    public RewardedAd( @NonNull ApplovinMaxMediationPlugin instance) {
-        this.instance = instance;
-    }
-
-    public void createRewardedAd(@NonNull String adUnitId,@NonNull Activity activity) {
-        rewardedAd = MaxRewardedAd.getInstance(adUnitId, activity);
-        rewardedAd.setListener(this);
-        rewardedAd.loadAd();
-    }
-
-    public boolean isReady() {
-        return rewardedAd.isReady();
-    }
-
-    public void showRewardedAd() {
-        rewardedAd.showAd();
-    }
-
-    // MAX Ad Listener
-    @Override
-    public void onAdLoaded(final MaxAd maxAd) {
-        // Rewarded ad is ready to be shown. rewardedAd.isReady() will now return 'true'
-        // Reset retry attempt
-        retryAttempt = 0;
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdLoaded", null);
-    }
-
-    @Override
-    public void onAdLoadFailed(final String adUnitId, final MaxError error) {
-        // Rewarded ad failed to load
-        // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
-        HashMap<String, String> err = new HashMap<>();
-        err.put("code", String.valueOf(error.getCode()));
-        err.put("message", error.getMessage().replace(":", ""));
-        instance.rewardedCallback(adUnitId, "onAdLoadFailed", err);
-
-        retryAttempt++;
-        long delayMillis = TimeUnit.SECONDS.toMillis((long) Math.pow(2, Math.min(6, retryAttempt)));
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                rewardedAd.loadAd();
-            }
-        }, delayMillis);
-    }
-
-    @Override
-    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error) {
-        // Rewarded ad failed to display. We recommend loading the next ad
-        HashMap<String, String> err = new HashMap<>();
-        err.put("code", String.valueOf(error.getCode()));
-        err.put("message", error.getMessage().replace(":", ""));
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdDisplayFailed", err);
-        rewardedAd.loadAd();
-    }
-
-    @Override
-    public void onAdDisplayed(final MaxAd maxAd) {
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdDisplayed", null);
-    }
-
-    @Override
-    public void onAdClicked(final MaxAd maxAd) {
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdClicked", null);
-    }
-
-
-    @Override
-    public void onAdHidden(final MaxAd maxAd) {
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdHidden", null);
-        // rewarded ad is hidden. Pre-load the next ad
-        rewardedAd.loadAd();
-    }
-
-
-    @Override
-    public void onRewardedVideoCompleted(final MaxAd maxAd) {
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onRewardedVideoCompleted", null);
-    }
-
-
-    @Override
-    public void onUserRewarded(final MaxAd maxAd, final MaxReward maxReward) {
-        instance.rewardedCallback(maxAd.getAdUnitId(), "onUserRewarded", null);
-        // Rewarded ad was displayed and user should receive the reward
-    }
-
-    @Override
-    public void onRewardedVideoStarted(MaxAd ad) {
-        instance.rewardedCallback(ad.getAdUnitId(), "onRewardedVideoStarted", null);
-    }
-}
+//package lk.dew.ads.applovinmax_mediation;
+//
+//import android.app.Activity;
+//import android.os.Handler;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.applovin.mediation.MaxAd;
+//import com.applovin.mediation.MaxError;
+//import com.applovin.mediation.MaxReward;
+//import com.applovin.mediation.MaxRewardedAdListener;
+//import com.applovin.mediation.ads.MaxRewardedAd;
+//
+//import java.util.HashMap;
+//import java.util.concurrent.TimeUnit;
+//
+//public class RewardedAd
+//        implements MaxRewardedAdListener {
+//    private MaxRewardedAd rewardedAd;
+//    private int retryAttempt;
+//    private ApplovinMaxMediationPlugin instance;
+//
+//    public RewardedAd( @NonNull ApplovinMaxMediationPlugin instance) {
+//        this.instance = instance;
+//    }
+//
+//    public void createRewardedAd(@NonNull String adUnitId,@NonNull Activity activity) {
+//        rewardedAd = MaxRewardedAd.getInstance(adUnitId, activity);
+//        rewardedAd.setListener(this);
+//        rewardedAd.loadAd();
+//    }
+//
+//    public boolean isReady() {
+//        return rewardedAd.isReady();
+//    }
+//
+//    public void showRewardedAd() {
+//        rewardedAd.showAd();
+//    }
+//
+//    // MAX Ad Listener
+//    @Override
+//    public void onAdLoaded(final MaxAd maxAd) {
+//        // Rewarded ad is ready to be shown. rewardedAd.isReady() will now return 'true'
+//        // Reset retry attempt
+//        retryAttempt = 0;
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdLoaded", null);
+//    }
+//
+//    @Override
+//    public void onAdLoadFailed(final String adUnitId, final MaxError error) {
+//        // Rewarded ad failed to load
+//        // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+//        HashMap<String, String> err = new HashMap<>();
+//        err.put("code", String.valueOf(error.getCode()));
+//        err.put("message", error.getMessage().replace(":", ""));
+//        instance.rewardedCallback(adUnitId, "onAdLoadFailed", err);
+//
+//        retryAttempt++;
+//        long delayMillis = TimeUnit.SECONDS.toMillis((long) Math.pow(2, Math.min(6, retryAttempt)));
+//
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                rewardedAd.loadAd();
+//            }
+//        }, delayMillis);
+//    }
+//
+//    @Override
+//    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error) {
+//        // Rewarded ad failed to display. We recommend loading the next ad
+//        HashMap<String, String> err = new HashMap<>();
+//        err.put("code", String.valueOf(error.getCode()));
+//        err.put("message", error.getMessage().replace(":", ""));
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdDisplayFailed", err);
+//        rewardedAd.loadAd();
+//    }
+//
+//    @Override
+//    public void onAdDisplayed(final MaxAd maxAd) {
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdDisplayed", null);
+//    }
+//
+//    @Override
+//    public void onAdClicked(final MaxAd maxAd) {
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdClicked", null);
+//    }
+//
+//
+//    @Override
+//    public void onAdHidden(final MaxAd maxAd) {
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onAdHidden", null);
+//        // rewarded ad is hidden. Pre-load the next ad
+//        rewardedAd.loadAd();
+//    }
+//
+//
+//    @Override
+//    public void onRewardedVideoCompleted(final MaxAd maxAd) {
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onRewardedVideoCompleted", null);
+//    }
+//
+//
+//    @Override
+//    public void onUserRewarded(final MaxAd maxAd, final MaxReward maxReward) {
+//        instance.rewardedCallback(maxAd.getAdUnitId(), "onUserRewarded", null);
+//        // Rewarded ad was displayed and user should receive the reward
+//    }
+//
+//    @Override
+//    public void onRewardedVideoStarted(MaxAd ad) {
+//        instance.rewardedCallback(ad.getAdUnitId(), "onRewardedVideoStarted", null);
+//    }
+//}
